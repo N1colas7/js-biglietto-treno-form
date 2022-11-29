7f08cbeaee4b1ae4b518e2c139c1ffea0fddcc63
N1colas7 <nicolasnd9897@gmail.com>
2022-11-29 10:44:41 +0100

    fix javascript and finish exercise

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
-bottone = document.getElementById("genera");
- bottone.addEventListener('click', function()
+const bottone = document.getElementById('genera');
+bottone.addEventListener('click', function()
 {
+let text = document.getElementById("myText").value;
 let biglietto_1;
 let km = document.getElementById('text-km').value;
 let eta = document.getElementById('text-age').value;
 
-if ( eta = 'maggiorenne' ) {
-    biglietto_1 =  (km * 0.21)   
-}else if ( eta = 'minorenne' ){
-    biglietto_1 = km*0.21
-    biglietto_1 -= (biglietto_1/100)*20
-}else( eta = 'ovessr-65' ) {
-    biglietto_1 = km*0.21
-    biglietto_1 -= (biglietto_1/100)*40
+if ( eta == 'over65' ){
+    biglietto_1 = (0.21 * km * 0.6).toFixed(2);
 }
+else if ( eta == 'minorenne' ){
+    biglietto_1 = (0.21 * km * 0.8).toFixed(2);
+} 
+else{
+    biglietto_1 =  (0.21 * km).toFixed(2);
+}    
 
-console.log(biglietto_1);
-
-const biglietto_1_readable = biglietto_1.toFixed(2)
-
-    document.getElementById("costo-biglietto").innerHTML = biglietto_1;
-    document.getElementById("myText").innerHTML = costo-biglietto;
+document.getElementById('nome-cognome').innerText = text
+document.getElementById('costo-biglietto').innerHTML = biglietto_1;
 } );
+
